788bc51f3c0f5c6e63c344b78dc43f580d513b98
shineharmn <707589431@qq.com>
2015-03-27 17:46:56 +0800

    for download
    server

--- src/com/engspeaker/controller/CasesController.java
+++ src/com/engspeaker/controller/CasesController.java
@@ -1,9 +1,14 @@
 package com.engspeaker.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,11 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.engspeaker.dao.ICasesDao;
-import com.engspeaker.dao.IUserDao;
 import com.engspeaker.dao.impl.CasesDaoImpl;
-import com.engspeaker.dao.impl.UserDaoImpl;
 import com.engspeaker.service.CasesService;
-import com.engspeaker.service.UserService;
 
 
 /**
@@ -23,7 +25,8 @@ import com.engspeaker.service.UserService;
  * @author shine
  *
  */
-@Controller("/cases.do")
+@Controller
+@RequestMapping("/cases.do")
 public class CasesController {
 	
 	private ICasesDao casesDao = new CasesDaoImpl();
@@ -49,6 +52,22 @@ public class CasesController {
 		}
 		
 		userService.addCase(options, question, answer); 
+		
+		return null;
+	}
+	
+	
+	@RequestMapping(params="method=download")
+	String download(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		
+		File file = new File("D:\\暂存\\图片2.png");
+		ServletOutputStream out = response.getOutputStream();
+		BufferedImage bi;
+		bi = ImageIO.read(file);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();   
+        ImageIO.write(bi, "png", baos);   
+        byte[] bytes = baos.toByteArray();   
+		out.write(bytes);
 		
 		return null;
 	}
